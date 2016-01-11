edec59c6ef58c4301a86b6df265094c9e842d671
Roberts <robertspencis@inbox.lv>
2016-01-11 16:55:20 +0200

    Pievienoju atkartoshanos funkciju

--- src/calcproper/CalcProper.java
+++ src/calcproper/CalcProper.java
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -17,8 +17,9 @@ public class CalcProper{
      * @param args the command line arguments
      */
      public static void main(String[] args) {
-        // TODO code application logic here         
-       
+        // TODO code application logic here   
+        int atkartot ;
+       do{
        Scanner kbinput = new Scanner(System.in);
        System.out.println("Ievadiet pirmo skaitli:");
        double a = kbinput.nextDouble();        
@@ -63,8 +64,11 @@ public class CalcProper{
                 break;
            default: 
                 System.out.println("Nederīga izvēle!");
-                break;
+                break;} 
+       System.out.println("vai velaties atkartot? 1 = Jā/ cits skaitlis = Nē");
+        Scanner in= new Scanner(System.in);
+         atkartot=in.nextInt();       
        }
-    
-    }
+       while (atkartot == 1);
+     }
 }
